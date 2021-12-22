f6f386f628f3268e4743fa3e67fe271cf04c998c
Woojin Joo <jwjin95@yonsei.ac.kr>
2021-12-23 01:40:22 +0900

    [feat] category menu bar, add bookmark component

--- src/components/dashboard/add-bookmark-component.tsx
+++ src/components/dashboard/add-bookmark-component.tsx
@@ -1,11 +1,46 @@
 import React, { useState } from "react";
 import { makeStyles } from "@mui/styles";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, TextField } from "@mui/material";
+import { AddCircleOutlineOutlined } from "@mui/icons-material";
 
 function AddBookmarkComponent() {
     const classes = useStyles();
+    const [open, setOpen] = useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const onAdd = (e:Event) => {
+
+    }
 
     return (
         <>
+            <IconButton onClick={handleOpen}>
+                <AddCircleOutlineOutlined fontSize="large" />
+            </IconButton>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>카테고리 추가</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        어떤 카테고리 추가하실?
+                    </DialogContentText>
+                    <TextField 
+                        variant="outlined"
+                        autoFocus
+                        margin="dense"    
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>OK</Button>
+                    <Button onClick={handleClose} color="warning">취소</Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 }
--- src/components/dashboard/menubar-component.tsx
+++ src/components/dashboard/menubar-component.tsx
@@ -1,18 +1,46 @@
 import React, { useState } from "react";
 import { makeStyles } from "@mui/styles";
+import { Accordion, AccordionDetails, AccordionSummary, Button, TextField, Typography } from "@mui/material"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 function MenubarComponent() {
     const classes = useStyles();
 
     return (
-        <>
-        </>
+        <div className={classes.root}>
+            <Accordion className={classes.menu} sx={{border:0.5}}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    
+                >
+                    <Typography>
+                        카테고리 목록
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Button>전체</Button>
+                    <Button color='primary'>
+                        OS
+                    </Button>
+                    <Button>CA</Button>
+                </AccordionDetails>
+            </Accordion>
+        </div>
+    
     );
 }
 
 const useStyles = makeStyles({
     root: {
-        color: "black"
+        color: "black",
+        display: "flex",
+        // flex: 3
+    },
+    menu: {
+        // display: "flex",
+        flex: 3,
+        // flexDirection: "column"
+        
     }
 });
 
