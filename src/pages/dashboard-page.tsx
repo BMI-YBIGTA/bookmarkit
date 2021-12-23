b8eefb66f3cedd762584186cad188143066a9b81
Woojin Joo <jwjin95@yonsei.ac.kr>
2021-12-24 03:00:54 +0900

    [feat] bookmarks in category mapping, with example [togo]: reducer implementation

--- src/pages/dashboard-page.tsx
+++ src/pages/dashboard-page.tsx
@@ -4,6 +4,36 @@ import AddBookmarkComponent from "../components/dashboard/add-bookmark-component
 import MenubarComponent from "../components/dashboard/menubar-component";
 import Header from "../components/header/header";
 import MainCategoryComponent from "../components/dashboard/main-category-component";
+import { ImageList } from "@mui/material";
+
+const reduced = [
+  {
+    mainCat: "CS",
+    bookmarks: [
+      {
+        smallCat: "OS",
+        title: "제목 111"
+      },
+      {
+        smallCat: "CA",
+        title: "제목 222"
+      }
+    ]
+  },
+  {
+    mainCat: "AI",
+    bookmarks: [
+      {
+        smallCat:"ML",
+        title: "머신러닝"
+      },
+      {
+        smallCat: "DL",
+        title: "딥러닝"
+      }
+    ]
+  }
+]
 
 function DashboardPage() {
   const classes = useStyles();
@@ -13,11 +43,20 @@ function DashboardPage() {
       <Header />
       <div className={classes.mainContainer}>
         <div className={classes.firstContainer}></div>
-
         <div className={classes.secondContainer}>
           <MenubarComponent />
           <AddBookmarkComponent />
-          <MainCategoryComponent />
+          <ImageList cols={2}>
+            {reduced.map((item) => {
+              return (
+                <MainCategoryComponent
+                  key={item.mainCat}
+                  mainCat={item.mainCat}
+                  bookmarks={item.bookmarks}
+                />
+              )
+            })}
+          </ImageList>
         </div>
         <div className={classes.thirdContainer}></div>
       </div>
